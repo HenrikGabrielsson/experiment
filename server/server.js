fbb0b10e936da72f51bebf0218797408cce19fe2
Henrik Gabrielsson <hg222aa@student.lnu.se>
2015-05-14 19:33:45 +0200

    websocket connecting to server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
-var websocketServer = require('websocket').server;
-var nodeStatic = require('node-static');
+var ws = require("websocket").server;
+var nodeStatic = require("node-static");
+var http = require("http");
 
-var server = new nodeStatic.Server('./public', {cache: 0});
+var fileServer = new nodeStatic.Server("./public", {cache: 0});
 
-require('http').createServer(function (request, response) 
+var httpServer =  http.createServer(function (request, response) 
 {
 	//capture timestamp when first request reaches server.
 	if(request.url === "/http.html" || request.url === "/ws.html")
@@ -11,12 +12,19 @@ require('http').createServer(function (request, response)
 		console.log(Date.now());
 	}
 
-	request.addListener('end', function () 
+	request.addListener("end", function () 
     {
-    	server.serve(request, response);
+    	fileServer.serve(request, response);
     }).resume();	
-	
-
 
 }).listen(8080);
 
+
+
+var wsServer = new ws({httpServer: httpServer});
+wsServer.on("request", function(request) {
+
+    var connection = request.accept("gallerytest", request.origin);
+
+    console.log("mu");
+});
