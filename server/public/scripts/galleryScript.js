391ef56ab2b8447c4ab6a1734d35ebd9ba8a17bb
Henrik Gabrielsson <hg222aa@student.lnu.se>
2015-05-14 18:13:07 +0200

    http.html klar? bytte från http-server till node-static

--- server/public/scripts/galleryScript.js
+++ server/public/scripts/galleryScript.js
@@ -1,1 +1,68 @@
-var test = 12;
+var changeGalleryView = function(e)
+{
+	if($(e.srcElement).hasClass("grid-view-image") === gridViewActive)
+	{
+		return;
+	}
+
+	gridViewActive = $(e.srcElement).hasClass("grid-view-image");
+
+	if(gridViewActive)
+	{
+		$(".grid-view-image").addClass("selected-view");
+		$(".full-view-image").removeClass("selected-view");
+
+		$("#gallery").append($(".gallery-image"));
+		$(".gallery-row").remove();
+
+		var row = document.createElement("div");
+		row.setAttribute("class", "row gallery-row");
+
+		$(".gallery-image").each(function(i, element)
+		{
+			//create new row every 3rd image.
+			if(i % 3 === 0)
+			{
+				row = document.createElement("div");
+				row.setAttribute("class", "row gallery-row");
+			}
+
+			$("#gallery").append($(row));
+			$(row).append(this);
+			$(this).removeClass("col-md-12");
+			$(this).addClass("col-md-4");
+		});
+
+	}
+	//full width images
+	else
+	{
+		$(".full-view-image").addClass("selected-view");
+		$(".grid-view-image").removeClass("selected-view");
+
+		$("#gallery").append($(".gallery-image"));
+		$(".gallery-row").remove();
+
+		$(".gallery-image").each(function(i, element)
+		{
+			var row = document.createElement("div");
+			row.setAttribute("class", "row gallery-row");
+
+			$("#gallery").append($(row));
+			$(row).append(this);
+			$(this).removeClass("col-md-4");
+			$(this).addClass("col-md-12");
+		});
+
+
+	}
+}
+var gridViewActive = true;
+
+var gridViewLink = document.getElementsByClassName("grid-view-link")[0];
+var fullViewLink = document.getElementsByClassName("full-view-link")[0];
+
+gridViewLink.addEventListener("click", changeGalleryView);
+fullViewLink.addEventListener("click", changeGalleryView);
+
+window.addEventListener("load", function(){console.log(Date.now());}, false);
--- server/server.js
+++ server/server.js
@@ -1,3 +1,22 @@
 var websocketServer = require('websocket').server;
-var httpServer = require('http-server');
+var nodeStatic = require('node-static');
+
+var server = new nodeStatic.Server('./public', {cache: 0});
+
+require('http').createServer(function (request, response) 
+{
+	//capture timestamp when first request reaches server.
+	if(request.url === "/http.html" || request.url === "/ws.html")
+	{
+		console.log(Date.now());
+	}
+
+	request.addListener('end', function () 
+    {
+    	server.serve(request, response);
+    }).resume();	
+	
+
+
+}).listen(8080);
 
